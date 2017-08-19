7bb8e6faeb90558b62d0738413c360f8c5094432
Maksim Kulikov <kulikovman@gmail.com>
2017-08-20 00:01:42 +0500

    Реализовано программное добавление кнопок

--- app/src/main/java/ru/kulikovman/steelratgame/MainActivity.java
+++ app/src/main/java/ru/kulikovman/steelratgame/MainActivity.java
@@ -4,16 +4,11 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.view.ContextThemeWrapper;
-import android.support.v7.widget.AppCompatButton;
-import android.support.v7.widget.LinearLayoutCompat;
-import android.view.ViewGroup.LayoutParams;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,14 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Инициализируем наши вью элементы
         mButtonContainer = (LinearLayout) findViewById(R.id.button_container);
-        mChapterText = (TextView) findViewById(R.id.chapter_text) ;
-
-        //
-        //LayoutParams layoutParams = new LinearLayoutCompat.LayoutParams(LayoutParams.MATCH_PARENT,
-                //LayoutParams.WRAP_CONTENT);
-
-
-
+        mChapterText = (TextView) findViewById(R.id.chapter_text);
 
         // Получаем SharedPreferences
         mSharedPref = getPreferences(Context.MODE_PRIVATE);
@@ -69,20 +57,40 @@ public class MainActivity extends AppCompatActivity {
     private void startGame() {
         switch (mChapterNumber) {
             case 0:
-                mChapterText.setText(R.string.ch_3);
+                mChapterText.setText(R.string.ch_0);
 
-                //ContextThemeWrapper newContext = new ContextThemeWrapper(this, R.style.MyButton);
-                //Button btn1 = new Button(this, null, R.style.MyButton);
-                Button button = new Button(new ContextThemeWrapper(this, R.style.MyButton), null, R.style.MyButton);
-
-
-                button.setText("Проверяем как работает эта чертова динамическая кнопка!");
-
-
-
-                mButtonContainer.addView(button);
+                addButton(getString(R.string.way_0_30), 30);
+                addButton(getString(R.string.way_1_229), 229);
 
                 break;
+            case 30:
+                mChapterText.setText(R.string.ch_30);
+
+                addButton(getString(R.string.way_0_30), 30);
+                addButton(getString(R.string.way_1_229), 229);
+                break;
+            case 229:
+                mChapterText.setText(R.string.ch_229);
+
+                addButton(getString(R.string.way_0_30), 30);
+                addButton(getString(R.string.way_1_229), 229);
+                break;
         }
+    }
+
+    private void addButton(String textButton, final int nextChapter) {
+        Button button = (Button) getLayoutInflater().inflate(R.layout.my_button, mButtonContainer, false);
+        button.setText(textButton);
+        mButtonContainer.addView(button);
+        Log.d("myLog", "Создали кнопку" + textButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mChapterNumber = nextChapter;
+                saveChapter();
+                startGame();
+            }
+        });
     }
 }
