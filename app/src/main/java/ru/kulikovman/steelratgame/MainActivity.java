49e041e00a18ccd3b92be402deedb691340671e3
Maksim Kulikov <kulikovman@gmail.com>
2017-08-20 08:39:38 +0500

    Переход в игровой активити

--- app/src/main/java/ru/kulikovman/steelratgame/MainActivity.java
+++ app/src/main/java/ru/kulikovman/steelratgame/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openGame(View view) {
-
+        Intent intent = new Intent(this, GameProcess.class);
+        startActivity(intent);
     }
 }
+
