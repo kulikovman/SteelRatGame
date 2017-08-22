b8b5d600bdc46a129d86e9da751da486c27f0936
Maksim Kulikov <kulikovman@gmail.com>
2017-08-22 12:26:10 +0500

    Кнопка "поставить оценку" теперь работает

--- app/src/main/java/ru/kulikovman/steelratgame/Rate.java
+++ app/src/main/java/ru/kulikovman/steelratgame/Rate.java
@@ -1,7 +1,10 @@
 package ru.kulikovman.steelratgame;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Rate extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Rate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
+    }
+
+    public void openAppPage(View view) {
+        // Переадресация на страницу приложения в маркете
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("market://details?id=ru.kulikovman.steelratgame"));
+        startActivity(intent);
     }
 }
